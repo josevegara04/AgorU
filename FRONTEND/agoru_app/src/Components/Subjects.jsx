53a233fb6d89e2de7236edaf2463747a8671bbd4
josevegara04 <jbvegar@eafit.edu.co>
2025-08-25 18:14:33 -0500

    add search bar

--- FRONTEND/agoru_app/src/Components/Subjects.jsx
+++ FRONTEND/agoru_app/src/Components/Subjects.jsx
@@ -3,14 +3,14 @@
 import React from "react";
 import "../Styles/Subjects.css";
 import { useState } from "react";
-import Reviews from "./Reviews"
+import Reviews from "./Reviews";
 import StudyMaterial from "./StudyMaterial";
 
 function Subjects({ email }) {
   const subjects = {
     "Semestre 1": [
       "Cálculo 1",
-      "Fundamentos de Progamación",
+      "Fundamentos de Programación",
       "Bienestar Universitario",
       "Principios de Desarrollo de Software",
       "Seminario de Ingenieria de Sistemas",
@@ -37,10 +37,44 @@ function Subjects({ email }) {
     subjects["Semestre 1"][0]
   );
   const [resource, setResource] = useState("studyMaterial");
+  const [searchTerm, setSearchTerm] = useState("");
+  const filteredSubjects = Object.values(subjects)
+    .flat()
+    .filter((subject) =>
+      subject.toLowerCase().includes(searchTerm.toLowerCase())
+    );
 
   return (
     <div className="subjects-container">
       <div className="subjects-sidebar">
+        <div className="search">
+          <label htmlFor="">Buscar</label>
+          <input
+            type="text"
+            className=""
+            value={searchTerm}
+            onChange={(e) => setSearchTerm(e.target.value)}
+          />
+          {searchTerm && (
+            <ul className="search-results">
+              {filteredSubjects.length > 0 ? (
+                filteredSubjects.map((subject) => (
+                  <li
+                    key={subject}
+                    onClick={() => {
+                      setSelectedSubject(subject);
+                      setSearchTerm(""); // limpiar búsqueda al seleccionar
+                    }}
+                  >
+                    {subject}
+                  </li>
+                ))
+              ) : (
+                <li>No se encontraron materias</li>
+              )}
+            </ul>
+          )}
+        </div>
         <h2> Semestres </h2>
         <ul>
           {Object.keys(subjects).map((semester) => (
@@ -84,7 +118,7 @@ function Subjects({ email }) {
       </div>
       <div className="subject-content">
         <h1>{selectedSubject}</h1>
-        {resource === "studyMaterial" ? <StudyMaterial/>: <Reviews />}
+        {resource === "studyMaterial" ? <StudyMaterial /> : <Reviews />}
       </div>
     </div>
   );
