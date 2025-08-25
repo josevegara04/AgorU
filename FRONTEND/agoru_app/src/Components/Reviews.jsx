2afcb9a3d077a5f6073b3950b7d4f2f033ce6e28
josevegara04 <jbvegar@eafit.edu.co>
2025-08-25 16:08:20 -0500

    new

---
+++ FRONTEND/agoru_app/src/Components/Reviews.jsx
@@ -0,0 +1,14 @@
+// Componentes de reseñas
+
+import React from "react";
+import "../Styles/reviews.css";
+
+function reviews(){
+    return(
+        <div className="">
+            <h1> Reseñas </h1>
+        </div>
+    )
+};
+
+export default reviews;
---
+++ FRONTEND/agoru_app/src/Components/StudyMaterial.jsx
@@ -0,0 +1,14 @@
+// Componentes de material de estudio
+
+import React from "react";
+import "../Styles/studyMaterial.css";
+
+function studyMaterial(){
+    return(
+        <div className="">
+            <h1> Material de estudio </h1>
+        </div>
+    )
+};
+
+export default studyMaterial;
