8065b9dedbdd4dfa36276abb4fd05fa9454092a7
josevegara04 <jbvegar@eafit.edu.co>
2025-10-17 10:00:35 -0500

    new component

---
+++ FRONTEND/agoru_app/src/Components/SummaryBar.jsx
@@ -0,0 +1,13 @@
+// Componente de la barra de resumen de las reseñas de cada materia
+
+import React from "react";
+import { useState } from "react";
+
+function SummaryBar() {
+    return(
+        <>
+        </>
+    )
+};
+
+export default SummaryBar;
