7da1b444c3d25bf261e65adacfa1d7a5686ba730
josevegara04 <jbvegar@eafit.edu.co>
2025-10-19 15:21:48 -0500

    markdown added

--- FRONTEND/agoru_app/src/Components/SummaryBar.jsx
+++ FRONTEND/agoru_app/src/Components/SummaryBar.jsx
@@ -3,18 +3,19 @@
 import React from "react";
 import { useState } from "react";
 import "../Styles/SummaryBar.css";
+import ReactMarkdown from "react-markdown";
 
 function SummaryBar({ onClose, summarizeText}) {
     
 
   return (
     <div className="summary-bar">
-      <button className="close-summary-button" onClick={onClose}>
+      <button className="btn btn-primary close-summary-button" onClick={onClose}>
         x
       </button>
       <h1>Resumen</h1>
       <div className="summary-text">
-        <p>{summarizeText}</p>
+        <ReactMarkdown>{summarizeText}</ReactMarkdown>
       </div>
     </div>
   );
