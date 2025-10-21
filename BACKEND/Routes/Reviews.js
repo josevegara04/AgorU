53f0070d9ae67c5b16d7e42984c100c1dd36e426
josevegara04 <jbvegar@eafit.edu.co>
2025-10-21 14:05:37 -0500

    fixed scroll problem

--- BACKEND/Routes/Reviews.js
+++ BACKEND/Routes/Reviews.js
@@ -1,5 +1,5 @@
 import express, { response } from "express";
-import db from "../db.js";
+import { openConnection, executeQuery, closeConnection} from "../db.js";
 import { authMiddleware } from "../index.js";
 
 const router = express.Router();
@@ -10,7 +10,7 @@ export async function fetchReviews(req, res, next) {
   const { idUser } = req.body;
 
   try {
-    const [result] = await db.query(
+    const result = await executeQuery(
       "SELECT review.*, user.email,ur.liked,ur.disliked FROM review JOIN user ON review.idPublisher = user.id LEFT JOIN user_review ur ON ur.idReview = review.id AND ur.idUser = ? WHERE review.idSubject = ?",
       [idUser, subjectId]
     );
@@ -36,7 +36,7 @@ router.post("/postReview", authMiddleware, async (req, res) => {
   const { idPublisher, content, idSubject } = req.body;
 
   try {
-    const [result] = await db.query(
+    const result = await executeQuery(
       "INSERT INTO review(idPublisher, content, postDate, idSubject, likes_count, dislikes_count) values(?, ?, NOW(), ?, 0, 0)",
       [idPublisher, content, idSubject]
     );
@@ -67,7 +67,7 @@ router.post("/handleLikes/review", async (req, res) => {
   }
 
   // Verificar que el usuario no haya reaccinado todavía a la reseña
-  const [query] = await db.query(
+  const query = await executeQuery(
     `SELECT ${reviewUserField} FROM user_review WHERE idUser = ? AND idReview = ?`,
     [idUser, idReview]
   );
@@ -76,13 +76,13 @@ router.post("/handleLikes/review", async (req, res) => {
     // No ha reaccionado
     try {
       // Actualizar el número de likes o dislikes de la reseña
-      const [result] = await db.query(
+      const result = await executeQuery(
         `UPDATE review SET ${change} = ${change}+1 WHERE id = ?`,
         [idReview]
       );
 
       // Crear un registro en la tabla user_review
-      const [update] = await db.query(
+      const update = await executeQuery(
         `INSERT INTO user_review(idUser, idReview, ${reviewUserField}) values(?, ?, 1)`,
         [idUser, idReview]
       );
@@ -94,68 +94,70 @@ router.post("/handleLikes/review", async (req, res) => {
   } else {
     // Ya reaccionó
     try {
+      const connection = await openConnection();
       if (reviewUserField === "liked") {
         if (query[0].liked === 1) {
           console.log("buenas");
-          await db.query("START TRANSACTION");
+          await connection.query("START TRANSACTION");
 
-          await db.query(
+          await connection.query(
             `DELETE FROM user_review WHERE idUser = ? AND idReview = ?`,
             [idUser, idReview]
           );
 
-          await db.query(
+          await connection.query(
             `UPDATE review SET likes_count = GREATEST(likes_count - 1, 0) WHERE id = ?`,
             [idReview]
           );
 
-          await db.query("COMMIT");
+          await connection.query("COMMIT");
         } else {
-          await db.query("START TRANSACTION");
+          await connection.query("START TRANSACTION");
 
-          await db.query(
+          await connection.query(
             `UPDATE user_review SET liked = 1, disliked = 0 WHERE idUser = ? AND idReview = ?`,
             [idUser, idReview]
           );
 
-          await db.query(
+          await connection.query(
             `UPDATE review SET likes_count = likes_count + 1, dislikes_count = GREATEST(dislikes_count - 1, 0) WHERE id = ?`,
             [idReview]
           );
 
-          await db.query("COMMIT");
+          await connection.query("COMMIT");
         }
       } else {
         if (query[0].disliked === 1) {
-          await db.query("START TRANSACTION");
+          await connection.query("START TRANSACTION");
 
-          await db.query(
+          await connection.query(
             `DELETE FROM user_review WHERE idUser = ? AND idReview = ?`,
             [idUser, idReview]
           );
 
-          await db.query(
+          await connection.query(
             `UPDATE review SET dislikes_count = GREATEST(dislikes_count - 1, 0) WHERE id = ?`,
             [idReview]
           );
 
-          await db.query("COMMIT");
+          await connection.query("COMMIT");
         } else {
-          await db.query("START TRANSACTION");
+          await connection.query("START TRANSACTION");
 
-          await db.query(
+          await connection.query(
             `UPDATE user_review SET liked = 0, disliked = 1 WHERE idUser = ? AND idReview = ?`,
             [idUser, idReview]
           );
 
-          await db.query(
+          await connection.query(
             `UPDATE review SET likes_count = GREATEST(likes_count - 1, 0), dislikes_count = dislikes_count + 1 WHERE id = ?`,
             [idReview]
           );
 
-          await db.query("COMMIT");
+          await connection.query("COMMIT");
         }
       }
+      await closeConnection(connection);
       return res.status(200).json({ message: "success" });
     } catch (err) {
       console.error(err);
