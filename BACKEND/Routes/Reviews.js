1c71be642420da9020bd685ea16bacf0fe2487d7
josevegara04 <jbvegar@eafit.edu.co>
2025-10-24 08:47:07 -0500

    fixed post comment query

--- BACKEND/Routes/Reviews.js
+++ BACKEND/Routes/Reviews.js
@@ -206,7 +206,7 @@ router.post("/postComment", authMiddleware, async (req, res) => {
 
   try {
     const result = await executeQuery(
-      "`INSERT INTO comment(idPublisher, content, postDate, idReview, likesCount, dislikesCount) values(?, ?, NOW(), ?, 0, 0)",
+      `INSERT INTO comment(idPublisher, content, postDate, idReview, likesCount, dislikesCount) values(?, ?, NOW(), ?, 0, 0)`,
       [idPublisher, content, idReview]
     );
     await executeQuery("UPDATE review SET comments_count = comments_count + 1 WHERE id = ?", [idReview]);
